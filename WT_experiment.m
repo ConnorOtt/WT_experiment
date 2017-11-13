c1961a004ff4d8d0b38b943fc804cc4469d6db27
Connor Ott <connor.ott@colorado.edu>
2017-11-13 16:54:31 -0700

    parse and align functions added

--- WT_experiment.m
+++ WT_experiment.m
@@ -58,12 +58,17 @@
 %
 %  Vocab hierarchy that I'm really gonna try to stick to:
 %       Experiment > Test > Trial == Sample > Data Point
-%   `   - There are sampleSize samples in a trial.
-%       - There are length(fullData) / sampleSize trials in a test.
-%       - There are probably like 2 or 3 tests in an experiment.
+%   `   - There are sampleSize data points in a trial (also called sample).
+%       - There are an arbitrary number of trials in a test, probably on
+%         order of 10 though (think changing angle of attack).
+%       - There are probably like 2 or 3 tests in an experiment (think 
+%         changing velocities).
+%       The last 2 can change from experiment to experiment, but there will
+%       always be sampleSize data points in a trial (or sample).
+%
 %
 % Created: 11/3/17 - Connor Ott
-% Last Modified: 11/10/17 - Connor Ott
+% Last Modified: 11/13/17 - Connor Ott
 %--------------------------------------------------------------------------
 
 classdef WT_experiment
@@ -265,7 +270,134 @@ classdef WT_experiment
             
             objMean = WT_experiment(dataSet, sSize);
         end
-       
+        % Parse or simply separate multiple or just one test from the rest.
+        function objCell = testParse(obj, parameterName, pVec)
+            %--------------------------------------------------------------
+            % Inputs
+            %      obj -       object getting parsed
+            %      parameter - String of parameter name. 
+            %      pVec -      Vector of parameter values by which function
+            %                  does the parsing.
+            % Outputs
+            %      cellArray - cell array of separated WT_experiment
+            %                  objects
+            %--------------------------------------------------------------
+            
+            name = parameterName;
+            parSepVec = obj.(name); % Parameter used for separation
+            
+            numTest = length(pVec);
+            objCell = cell(numTest, 1);
+            L_exp = length(parSepVec); % Length of entire experiment
+            L_test = L_exp / numTest; % Lenth of a single test
+            sSize = obj.sampleSize; % Sample size
+            
+            logMat = zeros(L_exp, numTest);
+            for i = 1:numTest
+                tempPVal = pVec(i);
+                logMat(:, i) = parSepVec >= tempPVal*0.9 & ...
+                               parSepVec <= tempPVal*1.1;
+                
+                p_idx = find(logMat(:, i));
+                temp1 = p_idx(1);     % first index of given paramter
+                tempEnd = p_idx(end); % last index of given parameter
+                
+                % Pulling out the full data set corresponding to the given
+                % parameter. 
+                
+                % fullData includes calibration data if there is any, don't
+                % want that getting mixed up in parsed data, hence the if 
+                % statement. 
+                if isempty(obj.dataCalibrate) 
+                    m = 1 + ((temp1 - 1) * sSize);
+                    n = (tempEnd * sSize);
+                    parsedDataSet =  obj.fullData(m:n, :);
+                else
+                    m = 1 + (temp1 + L_test - 1) * sSize;
+                    n = (tempEnd + L_test) * sSize;
+                    parsedDataSet = obj.fullData(m:n, :);
+                end
+                
+                objCell{i} = WT_experiment(parsedDataSet, sSize);
+                objCell{i}.fileName = [obj.fileName, ' - ', name, ...
+                                         ' = ', num2str(pVec(i))];
+            end
+            
+        end
+        % Combine two objects with odd and even angles of attack into a
+        % single object. 
+        function objZip = AoAzip(obj1, obj2)
+           %---------------------------------------------------------------
+           % To be used only with two objects wherein the only difference 
+           % between the two is that one contains odd angles of attack and
+           % the other contains the respective even angles of attack that
+           % would fit in between the others for the same wind tunnel
+           % conditions. 
+           %
+           % For instance, two groups take data at 25 m/s with the F-16
+           % model in its dirty configuration. One group takes data at odd
+           % angles of attack, the other at even angles of attack. This
+           % function will sort things out and zip it up like a zipper
+           % into a single object. 
+           %
+           % This will not work unless a scenario similar to the above is
+           % present (just use your head)
+           %---------------------------------------------------------------
+           
+           isOdd1 = mod(round(obj1.AoA(1)), 2);
+           isOdd2 = mod(round(obj2.AoA(1)), 2);
+           
+           if isOdd1 == isOdd2
+              error(['Both even and odd angle of attacked objects must',...
+                     ' be present']); 
+           else
+               if isOdd1
+                   oddObj = obj1;
+                   evenObj = obj2;
+               elseif isOdd2
+                   oddObj = obj2;
+                   evenObj = obj1;
+               end
+               
+               minOdd = min(oddObj.AoA);
+               minEven = min(evenObj.AoA);
+               
+               
+               L_exp = length(oddObj.AoA); % Experiment length
+               L_zipped = L_exp * 2; % Both experiments together
+               fullDataOdd = oddObj.fullData;
+               fullDataEven = evenObj.fullData;
+               sSize = oddObj.sampleSize;
+               [~, c] = size(fullDataOdd);
+               fullDataTotal = zeros(L_zipped, c);
+               
+               
+               for i = 1:L_zipped
+                   % For populating fullDataTotal -  i = 1, 2, 3, 4, ...
+                   m = 1 + (i-1) * sSize; 
+                   n = i * sSize;
+                   % For taking from even and odd - j = 1, 1, 2, 2, ...
+                   j = ceil(i/2);
+                   o = 1 + (j - 1) * sSize;
+                   p = j * sSize; 
+                   if minOdd < minEven % Start by pulling from odds
+                       if mod(i, 2) == 1 % pull from odds.
+                           fullDataTotal(m:n, :) = fullDataOdd(o:p, :);
+                       else
+                           fullDataTotal(m:n, :) = fullDataEven(o:p, :);
+                       end
+                   else % Start pulling from evens
+                       if mod(i, 2) == 1 % pull from evens.
+                           fullDataTotal(m:n, :) = fullDataEven(o:p, :);
+                       else
+                           fullDataTotal(m:n, :) = fullDataOdd(o:p, :);
+                       end
+                   end
+               end
+               
+               objZip = WT_experiment(fullDataTotal, sSize);
+           end
+        end
     end
     methods (Static)
         function [] = plot(obj, varargin)
